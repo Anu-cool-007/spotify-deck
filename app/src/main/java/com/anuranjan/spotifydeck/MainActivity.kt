e9249b8614a4e5fa2e379df542a6c52d36d090da
Anuranjan Srivastava <anuranjan.mail@gmail.com>
2021-08-07 15:01:27 +0530

    Initial commit

---
+++ app/src/main/java/com/anuranjan/spotifydeck/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.anuranjan.spotifydeck
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.anuranjan.spotifydeck.ui.theme.SpotifyDeckTheme
+import com.spotify.android.appremote.api.ConnectionParams
+import com.spotify.android.appremote.api.Connector.ConnectionListener
+import com.spotify.android.appremote.api.SpotifyAppRemote
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            SpotifyDeckTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SpotifyDeckTheme {
+        Greeting("Android")
+    }
+}
